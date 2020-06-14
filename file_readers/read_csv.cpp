6cf1a597df0a9a00876d48fe134971eec350ca36
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2020-06-14 17:09:14 +0300

    Update read_csv.cpp

--- file_readers/read_csv.cpp
+++ file_readers/read_csv.cpp
@@ -52,10 +52,8 @@ void read_csv(std::string path, std::vector<std::string> &markers) {
 
     fin.close();
     split(markers, str, '\n');
-    markers.resize(3340138);
-//    markers.resize(7000000);
+//    markers.resize(3340138);
 
-//    markers.resize(7000000);
 }
 
 
